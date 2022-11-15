a6dead69cfc45813d80785260bc5b231e35399fd
Fr4nkD3v <frank.code31@gmail.com>
2022-11-14 22:40:07 -0500

    responsive design added in initial and our section

---
+++ script.js
@@ -0,0 +1,16 @@
+// Variables
+const menuBtn = document.querySelector('header .menu');
+const lateralMenuPanel = document.querySelector('header .lateral-menu');
+
+// Handlers
+function handlerClickMenuBtn() {
+  menuBtn.classList.toggle('active');
+}
+
+// Event Listeners
+function eventListeners() {
+  
+  menuBtn.addEventListener('click', handlerClickMenuBtn);
+
+}
+eventListeners();
