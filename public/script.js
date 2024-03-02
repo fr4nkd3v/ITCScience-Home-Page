b6084c5d3adc78af4273f844ec9a96c552f25925
Fr4nkD3v <frank.code31@gmail.com>
2024-03-01 23:20:18 -0500

    chore: add links & improve hand animation

---
+++ public/script.js
@@ -0,0 +1,34 @@
+// Variables
+const menuBtn = document.querySelector('header .menu');
+const lateralMenuPanel = document.querySelector('header .lateral-menu');
+const handIcon = document.querySelector('.initial .header .hand')
+
+// Handlers
+function handlerClickMenuBtn() {
+  menuBtn.classList.toggle('active');
+}
+function setHandAnimation() {
+  if (handIcon.classList.contains('first-animate') || handIcon.classList.contains('animate'))
+    return
+
+  handIcon.classList.add('animate')
+  setTimeout(() => {
+    handIcon.classList.remove('first-animate')
+    handIcon.classList.remove('animate')
+  }, 1200)
+}
+
+// Event Listeners
+function eventListeners() {
+  menuBtn.addEventListener('click', handlerClickMenuBtn);
+  handIcon.addEventListener('click', setHandAnimation)
+}
+eventListeners();
+
+document.addEventListener('DOMContentLoaded', () => {
+  handIcon.classList.add('first-animate')
+  setTimeout(() => {
+    handIcon.classList.remove('first-animate')
+    handIcon.classList.remove('animate')
+  }, 2200)
+})
